5785f85f7734287692812ef408ea3c8ede9a8ddf
Denis Bain <baindenis95@gmail.com>
2024-02-07 09:41:34 -0300

    Add timepicker and colorpicker, fix barber selection

--- app.rb
+++ app.rb
@@ -13,7 +13,24 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.all
+end
+
+get '/' do
 	erb :index
 end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+		session[:user_name] = params[:username]
+		session[:phone] = params[:phone]
+		session[:datetime] = params[:datetime]
+		session[:barber] = params[:barber]
+		session[:color] = params[:color]
+
+		erb "<h1> Спасибо, вы записались!</h1>"
+	end
